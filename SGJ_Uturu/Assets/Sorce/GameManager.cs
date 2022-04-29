21faef0e31f2bcaf14a760c381d48bf4d27d2ce8
hazime0415 <kiryuu_hazime1@hotmail.com>
2022-04-29 20:27:47 +0900

    5体表示

--- SGJ_Uturu/Assets/Sorce/GameManager.cs
+++ SGJ_Uturu/Assets/Sorce/GameManager.cs
@@ -1,11 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Uturu
 {
     public class GameManager : MonoBehaviour
     {
+        public enum EnumGameStatus
+        {
+            Init,
+            Ready,
+            Play,
+            Result,
 
+            Max,
+        }
+        private EnumGameStatus m_gameStatus = EnumGameStatus.Init;
+
+        public const int SLEEPER_NUM = 5;
+        public const float STAGE_TIME = 30f;
+
+        [SerializeField, Tooltip("ゲームオブジェクト")] Transform m_gameTransform = null;
+        [SerializeField, Tooltip("寝てる人格納エリア")] Transform m_sleeperAreaTransform = null;
+
+        [SerializeField, Tooltip("スコア表示テキスト")] TextMeshProUGUI m_scoreText = null;
+        [SerializeField, Tooltip("時間表示テキスト")] TextMeshProUGUI m_timeText = null;
+
+        [SerializeField, Tooltip("プレイヤープレハブ")] PlayerController m_playerPrefab = null;
+        [SerializeField, Tooltip("寝てる人プレハブ")] SleeperController m_sleeperPrefab = null;
+
+        private PlayerController m_player = null;
+        private List<SleeperController> m_sleeperList = new List<SleeperController>();
+
+        public float m_time = STAGE_TIME;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            for(int i = 0; i < SLEEPER_NUM; i++)
+            {
+                SleeperController sleeper = Instantiate(m_sleeperPrefab, m_sleeperAreaTransform);
+                m_sleeperList.Add(sleeper);
+            }
+            m_player = Instantiate(m_playerPrefab, m_gameTransform);
+        }
+
+        private void Update()
+        {
+            switch (m_gameStatus)
+            {
+                case EnumGameStatus.Init:
+                    StatusInit();
+                    break;
+                case EnumGameStatus.Ready:
+                    StatusReady();
+                    break;
+                case EnumGameStatus.Play:
+                    StatusPlay();
+                    break;
+                case EnumGameStatus.Result:
+                    StatusResult();
+                    break;
+            }
+
+            m_timeText.text = string.Format("{0:00}", Mathf.Ceil(m_time));
+            m_scoreText.text = "0";
+        }
+
+        private void StatusInit()
+        {
+            m_gameStatus = EnumGameStatus.Ready;
+        }
+
+        private void StatusReady()
+        {
+            m_gameStatus = EnumGameStatus.Play;
+        }
+
+        private void StatusPlay()
+        {
+            m_time -= Time.deltaTime;
+
+            Mathf.Max(m_time, 0f);
+            if (m_time <= 0f)
+            {
+                m_gameStatus = EnumGameStatus.Result;
+            }
+        }
+
+        private void StatusResult()
+        {
+
+        }
     }
 }
--- SGJ_Uturu/Assets/Sorce/SleeperController.cs
+++ SGJ_Uturu/Assets/Sorce/SleeperController.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 namespace Uturu
 {
-    public class SleaperController : MonoBehaviour
+    public class SleeperController : MonoBehaviour
     {
         [System.Serializable]
-        public class SleaperStatusClass
+        public class SleeperStatusClass
         {
             public Sprite face = null;
             public float angle = 0f;
@@ -27,14 +27,19 @@ namespace Uturu
         public const float GAMEOVER_VALUE = 180f;
 
         [SerializeField, Tooltip("顔イメージ")] private Image m_faceImage = null;
-        [SerializeField, Tooltip("右足オブジェクト")] private GameObject m_rfootObj = null;
-        [SerializeField, Tooltip("左足オブジェクト")] private GameObject m_lfootObj = null;
+        [SerializeField, Tooltip("足オブジェクト")] private List<GameObject> m_footObjList = new List<GameObject>();
 
         [SerializeField, Tooltip("1秒に増える角度")] private float m_mag = 5.0f;
 
-        [SerializeField, Tooltip("各ステータス管理")] public List<SleaperStatusClass> m_statusList = new List<SleaperStatusClass>();
+        [SerializeField, Tooltip("各ステータス管理")] public List<SleeperStatusClass> m_statusList = new List<SleeperStatusClass>();
 
         private float m_value = 0f; // 足のつり具合
+
+        public RectTransform RectTransform { get; private set; } = null;
+        private void Awake()
+        {
+            RectTransform = GetComponent<RectTransform>();
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -48,19 +53,19 @@ namespace Uturu
             int mirror = m_moveFoot == (int)EnumMoveFoot.RightFoot ? -1 : 1;
 
             m_value += m_mag * Time.deltaTime;
-            m_rfootObj.transform.localEulerAngles = new Vector3(0f, 0f, m_value * mirror);
+            m_footObjList[m_moveFoot].transform.localEulerAngles = new Vector3(0f, 0f, m_value * mirror);
 
             // 顔変更
-            SleaperStatusClass sleaperStatus = m_statusList[0];
+            SleeperStatusClass sleeperStatus = m_statusList[0];
             for(int i = 0; i < m_statusList.Count; i++)
             {
                 if(m_value < m_statusList[i].angle)
                 {
                     break;
                 }
-                sleaperStatus = m_statusList[i];
+                sleeperStatus = m_statusList[i];
             }
-            m_faceImage.sprite = sleaperStatus.face;
+            m_faceImage.sprite = sleeperStatus.face;
         }
 
         public void ResetValue()
