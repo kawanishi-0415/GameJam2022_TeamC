20f97eb0a7b76705ceb557f723c6677baa39c41b
hazime0415 <kiryuu_hazime1@hotmail.com>
2022-04-29 18:54:33 +0900

    プロジェクト再生成
    無視リストがうまく機能していなかったのでプロジェクトを再生成しました。

---
+++ SGJ_Uturu/Assets/Sorce/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
+            transform.Translate(-3, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(3, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            
+        }
+    }
+}
