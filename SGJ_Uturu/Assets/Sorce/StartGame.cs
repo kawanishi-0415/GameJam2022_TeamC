a8c5d3d07564ae2a1bec77010ee4a468e26c8d64
ikasumi-hi <ymakekku@gmail.com>
2022-04-30 11:26:04 +0900

    kaisou

--- SGJ_Uturu/Assets/Sorce/StartGame.cs
+++ SGJ_Uturu/Assets/Sorce/StartGame.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public void LoadingNewScene()
-        {
-        if (Input.GetKeyDown(KeyCode.Space))
-        { SceneManager.LoadScene("Game"); }
+    public void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Game");
+            }
     }
 }
